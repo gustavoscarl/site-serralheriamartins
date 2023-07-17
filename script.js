39271c07bf5affd606a5b42b2aa9b88ac8a5aa40
Gustavo Scarcelli Lucianelli <104444836+gustavoscarl@users.noreply.github.com>
2023-07-17 12:46:11 -0300

    added smooth scroll and minor changes in style.css

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Slick.js config
+
 $(document).ready(function(){
   $('.autoplay').slick({
     slidesToShow: 1,
@@ -15,6 +17,7 @@ $(document).ready(function(){
   });
 });
 
+// Burger Menu
 
 const buttonBurger = document.querySelector('#burger');
       menu = document.querySelector('nav');
@@ -23,3 +26,22 @@ buttonBurger.addEventListener('click', () => {
   menu.classList.toggle('active')
 })
 
+// Scroll Link Interno
+
+const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href')
+  const section = document.querySelector(href)
+  const topo = section.offsetTop
+
+  window.scrollTo({
+    top: topo,
+    behavior: 'smooth',
+  });
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection)
+});
